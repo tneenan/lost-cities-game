22469590c166a9ac8f77da18601427e52fc2e775
Trevor Neenan <trevoraneenan@lewisu.edu>
2017-08-08 23:15:05 +0000

    Separate variables from main

--- app.py
+++ app.py
@@ -1,44 +1,5 @@
 import random
-
-player_hand = []
-opponent_hand = []
-player_yellow_stack = [] 
-player_blue_stack = [] 
-player_white_stack = []
-player_green_stack = []
-player_red_stack = []
-trade_yellow_stack = []
-trade_blue_stack = []
-trade_white_stack = []
-trade_green_stack = []
-trade_red_stack = []
-opponent_yellow_stack = []
-opponent_blue_stack = []
-opponent_white_stack = []
-opponent_green_stack = []
-opponent_red_stack = []
-colors = ['Yellow', 'Blue', 'White', 'Green', 'Red']
-
-opponent_board = [opponent_yellow_stack, opponent_blue_stack,
-                  opponent_white_stack, opponent_green_stack,
-                  opponent_red_stack]
-
-trading_board = [trade_yellow_stack, trade_blue_stack, trade_white_stack,
-                 trade_green_stack, trade_red_stack]
-
-game_board = [player_yellow_stack, player_blue_stack, player_white_stack,
-              player_green_stack, player_red_stack]
-              
-cards =  ['Yellow X', 'Yellow X', 'Yellow X', 'Yellow 2', 'Yellow 3', 'Yellow 4', 
-          'Yellow 5', 'Yellow 6', 'Yellow 7', 'Yellow 8', 'Yellow 9', 'Yellow 10',
-          'Blue X', 'Blue X', 'Blue X', 'Blue 2', 'Blue 3', 'Blue 4', 
-          'Blue 5', 'Blue 6', 'Blue 7', 'Blue 8', 'Blue 9', 'Blue 10',
-          'White X', 'White X', 'White X', 'White 2', 'White 3', 'White 4', 
-          'White 5', 'White 6', 'White 7', 'White 8', 'White 9', 'White 10',
-          'Green X', 'Green X', 'Green X', 'Green 2', 'Green 3', 'Green 4', 
-          'Green 5', 'Green 6', 'Green 7', 'Green 8', 'Green 9', 'Green 10',
-          'Red X', 'Red X', 'Red X', 'Red 2', 'Red 3', 'Red 4', 
-          'Red 5', 'Red 6', 'Red 7', 'Red 8', 'Red 9', 'Red 10']
+from variables import *
 
 def deal_cards():
     for i in xrange(8):
