ea5df84762fae8ed6b93cd25ef76a5b2ce24ed44
Trevor Neenan <trevoraneenan@lewisu.edu>
2017-08-08 04:30:38 +0000

    WIP: Add game rules to card play

--- app.py
+++ app.py
@@ -19,16 +19,16 @@ trading_board = [trade_yellow_stack, trade_blue_stack, trade_white_stack,
 game_board = [player_yellow_stack, player_blue_stack, player_white_stack,
               player_green_stack, player_red_stack]
               
-cards =  ['YellowX', 'YellowX', 'YellowX', 'Yellow2', 'Yellow3', 'Yellow4', 
-          'Yellow5', 'Yellow6', 'Yellow7', 'Yellow8', 'Yellow9', 'Yellow10',
-          'BlueX', 'BlueX', 'BlueX', 'Blue2', 'Blue3', 'Blue4', 
-          'Blue5', 'Blue6', 'Blue7', 'Blue8', 'Blue9', 'Blue10',
-          'WhiteX', 'WhiteX', 'WhiteX', 'White2', 'White3', 'White4', 
-          'White5', 'White6', 'White7', 'White8', 'White9', 'White10',
-          'GreenX', 'GreenX', 'GreenX', 'Green2', 'Green3', 'Green4', 
-          'Green5', 'Green6', 'Green7', 'Green8', 'Green9', 'Green10',
-          'RedX', 'RedX', 'RedX', 'Red2', 'Red3', 'Red4', 
-          'Red5', 'Red6', 'Red7', 'Red8', 'Red9', 'Red10']
+cards =  ['Yellow X', 'Yellow X', 'Yellow X', 'Yellow 2', 'Yellow 3', 'Yellow 4', 
+          'Yellow 5', 'Yellow 6', 'Yellow 7', 'Yellow 8', 'Yellow 9', 'Yellow 10',
+          'Blue X', 'Blue X', 'Blue X', 'Blue 2', 'Blue 3', 'Blue 4', 
+          'Blue 5', 'Blue 6', 'Blue 7', 'Blue 8', 'Blue 9', 'Blue 10',
+          'White X', 'White X', 'White X', 'White 2', 'White 3', 'White 4', 
+          'White 5', 'White 6', 'White 7', 'White 8', 'White 9', 'White 10',
+          'Green X', 'Green X', 'Green X', 'Green 2', 'Green 3', 'Green 4', 
+          'Green 5', 'Green 6', 'Green 7', 'Green 8', 'Green 9', 'Green 10',
+          'Red X', 'Red X', 'Red X', 'Red 2', 'Red 3', 'Red 4', 
+          'Red 5', 'Red 6', 'Red 7', 'Red 8', 'Red 9', 'Red 10']
 
 def deal_cards():
     for i in xrange(8):
@@ -47,7 +47,6 @@ def playing_card(player_hand):
             print 'Please choose a card in your hand.'
             chosen_card = raw_input('Which card would you like to play? ')
         else:
-            player_hand.remove(chosen_card)
             append_player_stack(chosen_card)
     if move_choice == 'trade':
         trade_card = raw_input('Which card would you like to trade away? ')
@@ -57,6 +56,7 @@ def playing_card(player_hand):
         else:
             player_hand.remove(trade_card)
             append_trade_stack(trade_card)
+            
 
 def append_trade_stack(trade_card):
     if 'Yellow' in trade_card:
@@ -72,15 +72,65 @@ def append_trade_stack(trade_card):
 
 def append_player_stack(chosen_card):
     if 'Yellow' in chosen_card:
-        player_yellow_stack.append(chosen_card)
+        if player_yellow_stack != []:
+            color, number = player_yellow_stack[-1].split(' ')
+            color2, number2 = chosen_card.split(' ')
+            if number == 'X':
+                player_yellow_stack.append(chosen_card)
+                player_hand.remove(chosen_card)
+            if number >= number2 and number != 'X':
+                print 'Unacceptable move. Please try again.'
+        else:
+            player_yellow_stack.append(chosen_card)
+            player_hand.remove(chosen_card)
     if 'Blue' in chosen_card:
-        player_blue_stack.append(chosen_card)
+        if player_blue_stack != []:
+            color, number = player_blue_stack[-1].split(' ')
+            color2, number2 = chosen_card.split(' ')
+            if number == 'X':
+                player_blue_stack.append(chosen_card)
+                player_hand.remove(chosen_card)
+            if number >= number2 and number != 'X':
+                print 'Unacceptable move. Please try again.'
+        else:
+            player_blue_stack.append(chosen_card)
+            player_hand.remove(chosen_card)
     if 'White' in chosen_card:
-        player_white_stack.append(chosen_card)
+        if player_white_stack != []:
+            color, number = player_white_stack[-1].split(' ')
+            color2, number2 = chosen_card.split(' ')
+            if number == 'X':
+                player_white_stack.append(chosen_card)
+                player_hand.remove(chosen_card)
+            if number >= number2 and number != 'X':
+                print 'Unacceptable move. Please try again.'
+        else:
+            player_white_stack.append(chosen_card)
+            player_hand.remove(chosen_card)
     if 'Green' in chosen_card:
-        player_green_stack.append(chosen_card)
+        if player_green_stack != []:
+            color, number = player_green_stack[-1].split(' ')
+            color2, number2 = chosen_card.split(' ')
+            if number == 'X':
+                player_green_stack.append(chosen_card)
+                player_hand.remove(chosen_card)
+            if number >= number2 and number != 'X':
+                print 'Unacceptable move. Please try again.'
+        else:
+            player_green_stack.append(chosen_card)
+            player_hand.remove(chosen_card)
     if 'Red' in chosen_card:
-        player_red_stack.append(chosen_card)
+        if player_red_stack != []:
+            color, number = player_red_stack[-1].split(' ')
+            color2, number2 = chosen_card.split(' ')
+            if number == 'X':
+                player_red_stack.append(chosen_card)
+                player_hand.remove(chosen_card)
+            if number >= number2 and number != 'X':
+                print 'Unacceptable move. Please try again.'
+        else:
+            player_red_stack.append(chosen_card)
+            player_hand.remove(chosen_card)
 
 def draw_card(player_hand):
     while len(player_hand) < 8:
@@ -95,10 +145,7 @@ def draw_card(player_hand):
                 else:
                     player_hand.append(pickup_card)
                     desired_stack.remove(pickup_card)
-            else:
-                selection = random.choice(cards)
-                player_hand.append(selection)
-        else:
+        if trading_board == [[],[],[],[],[]] or trading == 'no':
             selection = random.choice(cards)
             player_hand.append(selection)
         
